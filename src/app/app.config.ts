e32dd6b2f26eb3db9a10ff0b3e8000522b070a63
tomas-barros1 <tomasabbarros3@gmail.com>
2025-07-18 11:41:37 -0300

    projeto criado

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig, provideAppInitializer } from "@angular/core";
+
+export const appConfig: ApplicationConfig = {
+  providers: [],
+};
